501a286c3d8d98da04296939e6b81b11f58908d3
serwer20 <serwer20@gmail.com>
2022-07-03 13:52:30 +0200

    [fix] missing temp folder

--- src/utils.js
+++ src/utils.js
@@ -1,12 +1,21 @@
+const fs = require("fs");
+
 const calculateProfit = (oldVal, newVal) => ((newVal - oldVal) / oldVal) * 100;
 
-const toDecimal = (number, decimals) =>
-	parseFloat(number / 10 ** decimals).toFixed(decimals);
+const toDecimal = (number, decimals) => parseFloat(number / 10 ** decimals).toFixed(decimals);
 
 const toNumber = (number, decimals) => number * 10 ** decimals;
+
+const createTempDir = () => {
+	// create a 'temp' directory if not exists
+	if (!fs.existsSync("./temp")) {
+		fs.mkdirSync("./temp");
+	}
+};
 
 module.exports = {
 	calculateProfit,
 	toDecimal,
 	toNumber,
+	createTempDir,
 };
