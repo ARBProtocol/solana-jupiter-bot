dcbffe130ee494b2de303e6c0ee6d180fadb9ac5
serwer20 <serwer20@gmail.com>
2022-07-10 17:18:50 +0200

    debug version for testing purposes

--- src/swap.js
+++ src/swap.js
@@ -1,15 +1,31 @@
 const { calculateProfit, toDecimal } = require("./utils");
 const cache = require("./cache");
+const fs = require("fs");
 
 const swap = async (jupiter, route) => {
 	try {
 		const performanceOfTxStart = performance.now();
 		cache.performanceOfTxStart = performanceOfTxStart;
 
+		// save route object to ./temp/routeInfoBeforeSwap.json
+		await fs.writeFile(
+			"./temp/routeInfoBeforeSwap.json",
+			JSON.stringify(route, null, 2),
+			() => {}
+		);
+
 		const { execute } = await jupiter.exchange({
 			routeInfo: route,
 		});
+
 		const result = await execute();
+
+		// save result object to ./temp/result.json
+		await fs.writeFile(
+			"./temp/result.json",
+			JSON.stringify(await result, null, 2),
+			() => {}
+		);
 
 		const performanceOfTx = performance.now() - performanceOfTxStart;
 
@@ -37,50 +53,73 @@ const successSwapHandler = (tx, tradeEntry, tokenA, tokenB) => {
 	// update counter
 	cache.tradeCounter[cache.sideBuy ? "buy" : "sell"].success++;
 
-	// update balance
-	if (cache.sideBuy) {
-		cache.lastBalance.tokenA = cache.currentBalance.tokenA;
-		cache.currentBalance.tokenA = 0;
-		cache.currentBalance.tokenB = tx.outputAmount;
-	} else {
-		cache.lastBalance.tokenB = cache.currentBalance.tokenB;
-		cache.currentBalance.tokenB = 0;
-		cache.currentBalance.tokenA = tx.outputAmount;
-	}
+	if (cache.config.tradingMode === "pingpong") {
+		// update balance
+		if (cache.sideBuy) {
+			cache.lastBalance.tokenA = cache.currentBalance.tokenA;
+			cache.currentBalance.tokenA = 0;
+			cache.currentBalance.tokenB = tx.outputAmount;
+		} else {
+			cache.lastBalance.tokenB = cache.currentBalance.tokenB;
+			cache.currentBalance.tokenB = 0;
+			cache.currentBalance.tokenA = tx.outputAmount;
+		}
 
-	// update profit
-	if (cache.sideBuy) {
-		cache.currentProfit.tokenA = 0;
-		cache.currentProfit.tokenB = calculateProfit(
-			cache.initialBalance.tokenB,
-			cache.currentBalance.tokenB
+		// update profit
+		if (cache.sideBuy) {
+			cache.currentProfit.tokenA = 0;
+			cache.currentProfit.tokenB = calculateProfit(
+				cache.initialBalance.tokenB,
+				cache.currentBalance.tokenB
+			);
+		} else {
+			cache.currentProfit.tokenB = 0;
+			cache.currentProfit.tokenA = calculateProfit(
+				cache.initialBalance.tokenA,
+				cache.currentBalance.tokenA
+			);
+		}
+
+		// update trade history
+		let tempHistory = cache.tradeHistory;
+
+		tradeEntry.inAmount = toDecimal(
+			tx.inputAmount,
+			cache.sideBuy ? tokenA.decimals : tokenB.decimals
 		);
-	} else {
-		cache.currentProfit.tokenB = 0;
+		tradeEntry.outAmount = toDecimal(
+			tx.outputAmount,
+			cache.sideBuy ? tokenB.decimals : tokenA.decimals
+		);
+
+		tradeEntry.profit = calculateProfit(
+			cache.lastBalance[cache.sideBuy ? "tokenB" : "tokenA"],
+			tx.outputAmount
+		);
+		tempHistory.push(tradeEntry);
+		cache.tradeHistory = tempHistory;
+	}
+	if (cache.config.tradingMode === "arbitrage") {
+		cache.lastBalance.tokenA = cache.currentBalance.tokenA;
+		cache.currentBalance.tokenA = tx.outputAmount;
+
 		cache.currentProfit.tokenA = calculateProfit(
 			cache.initialBalance.tokenA,
 			cache.currentBalance.tokenA
 		);
+
+		// update trade history
+		let tempHistory = cache.tradeHistory;
+
+		tradeEntry.inAmount = toDecimal(tx.inputAmount, tokenA.decimals);
+		tradeEntry.outAmount = toDecimal(tx.inputAmount, tokenA.decimals);
+
+		tradeEntry.profit = calculateProfit(
+			cache.lastBalance["tokenA"],
+			tx.outputAmount
+		);
+		tempHistory.push(tradeEntry);
+		cache.tradeHistory = tempHistory;
 	}
-
-	// update trade history
-	let tempHistory = cache.tradeHistory;
-
-	tradeEntry.inAmount = toDecimal(
-		tx.inputAmount,
-		cache.sideBuy ? tokenA.decimals : tokenB.decimals
-	);
-	tradeEntry.outAmount = toDecimal(
-		tx.outputAmount,
-		cache.sideBuy ? tokenB.decimals : tokenA.decimals
-	);
-
-	tradeEntry.profit = calculateProfit(
-		cache.lastBalance[cache.sideBuy ? "tokenB" : "tokenA"],
-		tx.outputAmount
-	);
-
-	tempHistory.push(tradeEntry);
-	cache.tradeHistory = tempHistory;
 };
 exports.successSwapHandler = successSwapHandler;
