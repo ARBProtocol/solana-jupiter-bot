9cd024d8ea94cb2393f623f1eaae6e4ae9fedef6
serwer20 <serwer20@gmail.com>
2022-07-24 14:38:11 +0200

    add Network page

--- src/wizard/Pages/Network.js
+++ src/wizard/Pages/Network.js
@@ -2,12 +2,27 @@ const React = require("react");
 const { Box, Text } = require("ink");
 const WizardContext = require("../WizardContext");
 const { useContext } = require("react");
+const { default: SelectInput } = require("ink-select-input");
+
+const NETWORKS = [
+	{ label: "mainnet-beta", value: "mainnet-beta" },
+	{ label: "testnet", value: "testnet" },
+	{ label: "devnet", value: "devnet" },
+];
 
 function Network() {
-	const { nav } = useContext(WizardContext);
+	const { config, configSetValue } = useContext(WizardContext);
 	return (
-		<Box>
-			<Text>NETWORK COMPONENT</Text>
+		<Box flexDirection="column">
+			<Text>
+				Select Solana <Text color="magenta">Network</Text>:
+			</Text>
+			<Box margin={1}>
+				<SelectInput
+					items={NETWORKS}
+					onSelect={(item) => configSetValue("network", item.value)}
+				/>
+			</Box>
 		</Box>
 	);
 }
