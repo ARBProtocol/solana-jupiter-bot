d43b9447c5df6f3c4abc06c0b1a0303802545aba
Paweł Mioduszewski <pmioduszewski@dobrebranie.pl>
2023-03-17 22:15:11 +0100

    add log level formatter

--- packages/core/src/logger.ts
+++ packages/core/src/logger.ts
@@ -1,8 +1,15 @@
 /* eslint-disable turbo/no-undeclared-env-vars */
-// TODO: fix getting LOG_LEVEL from .env, maybe consume it during bot creation and store it in zustand (?)
+// TODO: fix getting LOG_LEVEL from .env, maybe consume it during bot creation and store it in zustand
 import pino from "pino";
 
 export const logger = pino(
-	{ level: process.env.LOG_LEVEL || "info" },
+	{
+		level: process.env.LOG_LEVEL || "info",
+		formatters: {
+			level: (label) => ({
+				level: label,
+			}),
+		},
+	},
 	pino.destination(`./temp/bot.log`)
 );
