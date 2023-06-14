7ed99bfd5e360af5182f0a2a56815b4d60c221e3
Paweł Mioduszewski <pmioduszewski@dobrebranie.pl>
2023-06-14 19:20:29 +0200

    core: drop jup-ag/core dependency core: remove deprecated code

--- packages/core/src/services/aggregators/jupiter/tokens.ts
+++
@@ -1,9 +0,0 @@
-import { JupiterToken, TOKEN_LIST_URL } from "./jupiter";
-import axios from "axios";
-
-export const getJupiterTokens = async () => {
-	const response = await axios.get(TOKEN_LIST_URL["mainnet-beta"]);
-	const data = response.data;
-	if (!data) throw new Error("getJupiterTokens: No tokens found - empty data");
-	return data as JupiterToken[] | undefined;
-};
